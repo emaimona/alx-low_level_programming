23e83959163a6c3b22268043567db919ec2a489a
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-21 23:45:22 +0100

    add file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print all number of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (; n < 10; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
