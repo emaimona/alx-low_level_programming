bccfb1b1e3352b5ab55948079b21aa38e16d78c8
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-21 23:32:02 +0100

    add file 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase, except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l != 'q' || l != 'e')
+			putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
