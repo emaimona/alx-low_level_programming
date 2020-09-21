1e07f4540322d6f29720099b63970082d77e393e
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-22 00:52:43 +0100

    add file 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,14 @@ int main(void)
 {
 	int n = '0';
 
-	for (; n <= '9'; n++)
+	while (n <= '9')
 	{
 		putchar(n);
+		if (n == '9')
+			break;
 		putchar(',');
 		putchar(' ');
+		n++;
 	}
 	putchar('\n');
 	return (0);
