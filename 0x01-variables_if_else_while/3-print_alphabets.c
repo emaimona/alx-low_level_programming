fdf7a79d84ff85b7718763b1daec595f9c1c71cb
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-21 23:24:33 +0100

    add file 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lower and uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+	for (l = 'A'; l <= 'Z'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
