78d271c0c18e07a825fec8512741799518490d9f
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-22 00:30:46 +0100

    add file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in lowercase reversively
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
