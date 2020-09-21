a9547c1f1a94512f1e317ac9a8d00c349b586425
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-21 23:13:55 +0100

    add file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in lowercase with putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
