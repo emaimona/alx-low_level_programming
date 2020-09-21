d0f7639cad3b6ff002587936a683b2ab56698212
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-22 00:42:28 +0100

    add file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print all number of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	char n = '0';
+
+	for (; n <= '9'; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
