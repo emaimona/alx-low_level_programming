27848ad09a0302cc702bf835756447615b169c4f
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-22 21:01:04 +0100

    add file 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <ctype.h>
+
+/**
+ * _islower - check if a char is lowercase
+ * @c: the value
+ * Return: 1 if true, 0 if false
+ */
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+}
