01eaed80ca2805b66b451998804d10c77b91cb77
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-23 20:45:20 +0100

    add file 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,8 +11,18 @@ int print_last_digit(int v)
 	int r = 0;
 
 	if (v < 0)
+	{
 		v *= (-1);
-	r = v % 10;
+		r = v % 10;
+	}
+	else if ( v == 0 || v == 1)
+	{
+		r = v;
+	}
+	else
+	{
+		r = v % 10;
+	}
 	_putchar(r + '0');
 	return (r);
 }
