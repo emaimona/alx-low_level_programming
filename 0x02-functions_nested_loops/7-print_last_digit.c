eee2ca26ec5c4e2d01ea1e66176e39877331f75c
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-23 20:20:36 +0100

    add file 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @v: value of the digit
+ * Return: the last digit
+ */
+
+int print_last_digit(int v)
+{
+	return (v % 10);
+}
