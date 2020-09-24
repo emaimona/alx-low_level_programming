e9c8c0167bd3e157aa039e3d1af734434be249bf
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-24 21:30:42 +0100

    add file 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print '\' n number of times
+ * @n: The number of times
+ * Return: Always 0
+ */
+
+void print_diagonal(int n)
+{
+	int c = 0;
+
+	for (; c < n; c++)
+	{
+		if (n > 0)
+			_putchar('\\');
+	}
+	_putchar('\n');
+
+}
