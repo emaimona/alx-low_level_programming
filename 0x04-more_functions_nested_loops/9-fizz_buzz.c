f80aa35fb6d2b55cbf930d8ca98bd31bd43a91e9
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-26 14:23:12 +0100

    add file 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,15 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3) == 0 && (n % 5) == 0)
-		{
 			printf("FizzBuzz ");
-		} else if (n % 3 == 0)
-		{
+		else if (n % 3 == 0)
 			printf("Fizz ");
-		} else if (n % 5 == 0)
-		{
+		else if (n % 5 == 0)
 			printf("Buzz ");
-		} else
-		{
+		else
 			printf("%d ", n);
-		}
 	}
-
-	printf("\n");
+		printf("\n");
 
 	return (0);
 }
