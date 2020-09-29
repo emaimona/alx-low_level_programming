698501c71e55bfb9534f0d791ce280ec2f7e7c85
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-29 02:29:41 +0100

    add file 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - resets the value passed to 98
+ * @n: the value to be changed
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	n = 98;
+	_putchar('\n');
+}
