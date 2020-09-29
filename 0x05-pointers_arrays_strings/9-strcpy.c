7fbad130333d61b3fd064fcba519673c1895ec4f
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-29 21:58:05 +0100

    add file 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - copies the string pointed to
+ * @dest: the buffer pointed to
+ * @src: the string to be copied
+ * Return: the value of @dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
