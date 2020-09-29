1460b8603c6fb4c43608c05dc990f563f2f21551
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-29 20:52:26 +0100

    add file 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints a half of a given string
+ * @str: the string value
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int v, n;
+
+	for (v = 0; *(str + v) != '\0'; v++)
+		;
+	if (v % 2 == 0)
+		n = v / 2;
+	else
+		n = (v - 1) / 2;
+
+	for (; n <= v; n++)
+		_putchar(*(str + n));
+
+	_putchar('\n');
+}
