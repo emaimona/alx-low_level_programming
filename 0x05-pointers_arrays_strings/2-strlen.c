511928d65f0070baf4c4ff63a0db5745a6b75589
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-29 03:12:21 +0100

    add file 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the lenght of a given string
+ * @s - the string value
+ * Return: The lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int v = 0;
+
+	while (v >= 0)
+	{
+		if (*(s + v) == '\0')
+			break;
+		v++;
+	}
+	return (v);
+}
