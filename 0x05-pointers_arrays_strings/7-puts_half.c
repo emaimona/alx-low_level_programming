264e461a438e2b0921d9b9991325906de6ff79f6
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-09-30 22:08:23 +0100

    add file 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,10 +16,9 @@ void puts_half(char *str)
 		n = v / 2;
 	else
 		n = (v - 1) / 2;
-	for (; n < v; n++)
+	for (; n <= v; n++)
 	{
-		if (n % v != 0)
-			_putchar(*(str + n));
+		_putchar(*(str + n));
 	}
 	_putchar('\n');
 }
