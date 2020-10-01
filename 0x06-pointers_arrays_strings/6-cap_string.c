b720b0fb0b5e04a012bb41774c0f1f5ad506cc7d
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-10-01 21:59:31 +0100

    add file 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * *cap_string - capitalizes all words
+ * @s: the string
+ * Return: the capitalizes of the string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+	char c = 'a';
+
+	for (; *(s + i) != '\0'; i++)
+	{
+		if ( *(s + i) != '\t' && *(s + i) != ' ' && *(s + i) != '\n' && *(s + i) != ',' && *(s + i) != ';' && *(s + i) != '.' && *(s + i) != '!' && *(s + i) != '?' && *(s + i) != '"' && *(s + i) != '(' && *(s + i) != ')' && *(s + i) != '{' && *(s + i) != '}')
+		{
+			if (*(s + i) >= 'A' && *(s + i) <= 'Z')
+			{
+				for (c = 'A'; c <= 'Z'; c++)
+				{
+					if (*(s + i) == c)
+						*(s + i) = c + 32;
+				}
+			}
+		}
+		else
+		{
+			if (*(s + (i + 1)) != ' ')
+			{
+				for (c = 'a'; c <= 'z'; c++)
+				{
+					if (*(s + (i + 1)) == c)
+						*(s + (i + 1)) = c - 32;
+				}
+			}
+			else
+			{
+				for (c = 'a'; c <= 'a'; c++)
+				{
+					if (*(s + (i + 0)) == c)
+						*(s + (i + 0)) = c - 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
