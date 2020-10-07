2247ec9055d6854ff34138431de3d3e191eec0ae
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-10-07 21:41:58 +0100

    add file 100

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: 1 if the strings can be considered identical, otherwise return 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+
+}
