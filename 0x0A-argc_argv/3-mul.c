8703938844e2acac41c1fcb27ecf79dcb05c6a39
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-10-11 18:43:46 +0100

    add file 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two
+ * @argc: args counter
+ * @argv: args vector
+ * Return: 0 (sucess) an 1 (Error)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
