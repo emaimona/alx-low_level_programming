6852c7ef05de7046fe9d69dfad4fd4da748e8caa
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-10-11 19:43:31 +0100

    add file 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
+
+/**
+ * _isnum - checks for a digit
+ * @n: number to be checked
+ * Return: 0 or 1
+ */
+int _isnum(char n[])
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (isdigit(n[i]) == 0)
+			return (0);
+	}
+	return (1);
+}
+
 /**
  * main - add positive numbers
  * @argc: args counter
@@ -15,17 +34,17 @@ int main(int argc, char *argv[])
 		printf("0\n");
 	else
 	{
-	for (; c <= argc && argv[argc] == '\0'; c++)
-	{
-		if (!isdigit(argv[c]))
+		for (; c < argc; c++)
 		{
-			printf("Error\n");
-			return (1);
+			if (_isnum(argv[c]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum += atoi(argv[c]);
 		}
-		else
-			sum += argv[v];
+		printf("%d\n", sum);
 	}
-	printf("%d\n", sum);
 	return (0);
-	}
 }
