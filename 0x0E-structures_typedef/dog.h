595ad0ec33cc60ffd1efa8492a27b87eba017940
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-10-19 17:07:53 +0100

    add dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct for a dog id
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+int _putchar(char c);
+
+#endif
