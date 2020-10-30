dd304b88adb0d6c7fbc11cca0605f55aa083c3da
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-10-30 16:11:47 +0100

    add file 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _strlen(const char *s);
+
 /**
  * *add_node - adds a new node at the beginning of list_t
  * @head: The node to be added
@@ -24,9 +26,24 @@ list_t *add_node(list_t **head, const char *str)
 		free(node);
 		return (NULL);
 	}
-	node->len = strlen(node->str);
+	node->len = _strlen(node->str);
 	node->next = *head;
 
 	*head = node;
 	return (node);
+}
+
+/**
+ * _strlen - returns length of string
+ * @s: character of string
+ * Return: length of string
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	while (s[i] != 0)
+		i++;
+	return (i);
 }
