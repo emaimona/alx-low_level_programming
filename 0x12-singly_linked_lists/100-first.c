9974b9981c48c2e8b29cdb6c9664755ee2477ccb
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-10-30 22:00:04 +0100

    add file 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void first(void) __atribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 
 /**
- * first - Prints befor main
+ * first - prints something before main
  *
+ * Return: void
  */
-
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
