c47745e409c3ca62ab444364062a2f837c69b237
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-11-04 19:59:42 +0100

    add file 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -14,9 +14,10 @@ int pop_listint(listint_t **head)
 	listint_t *temp = *head;
 	int n;
 
-	if (temp == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 
+	*head = temp->next;
 	n = temp->n;
 	free(temp);
 
