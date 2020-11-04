45475f3d4fdc40e7dc0439fa63067ecdd3e3a3d1
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-11-04 20:52:25 +0100

    add file 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,7 +17,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	for (; i < index, n++)
+	for (; i < index; i++)
 	{
 		if (head == NULL)
 			return (NULL);
