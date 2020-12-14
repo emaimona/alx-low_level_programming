4ae1c11a15bfe031375ddb25a798faea8c761c90
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-12-14 17:18:24 +0100

    add 100-operations.so file

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - function that adds two integers and returns the result.
+ * @a: input integer 1
+ * @b: input integer 2
+ * Return: int.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
