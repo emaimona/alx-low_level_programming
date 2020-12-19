1db8c558906b72a8018c2dc0ae66673ad63bb2b3
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-12-19 18:59:11 +0100

    add file 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - inserts a new node at a given position.
+ * @head: The node pointer to the struct
+ * @index: The index of the node that should be deleted.
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
+	{
+		if (temp == NULL)
+			return (-1);
+		temp = temp->next;
+	}
+
+	if (temp == *head)
+	{
+		*head = temp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		temp->prev->next = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = temp->prev;
+	}
+
+	free(temp);
+
+	return (1);
+}
